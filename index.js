deb2e56d50445e78820e2638e8f00a2a99890ef9
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2025-01-27 07:24:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 6000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Pipeline Testing 2!')
+  response.send('Pipeline Testing 3!')
 })
 
 app.listen(app.get('port'), function() {
