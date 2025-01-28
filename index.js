edc29d59017fb19530b714ea26fffeb91ba0dc1a
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2025-01-27 19:07:30 -0500

    Fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 6000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Pipeline Testing 18!')
+  response.send('Pipeline Testing 19!')
 })
 
 app.listen(app.get('port'), function() {
